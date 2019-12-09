1a1dd2f1d256bb1ce84faf284fe91770a7c7bad0
murray <muweilaw@qq.com>
2019-12-09 19:50:18 +0800

    新增了对智联招聘简历的技能特长解析功能

--- src/main/java/com/lds/common/resume/parser/SkillsParser.java
+++ src/main/java/com/lds/common/resume/parser/SkillsParser.java
@@ -21,11 +21,29 @@ public class SkillsParser extends BaseParser {
 
     public List<Skills> parse() {
         ArrayList<Skills> list = new ArrayList<>();
+//        找到51job的技能特长模块全部元素
         Elements skills51Eles = root.getElementsMatchingOwnText("^技能特长$");
-        Elements skillsEles = root.getElementsMatchingOwnText("^技能特长$");
-
-        if (skillsEles.size() > 0) {
-            Element educationEleParent = skillsEles.first().parent().nextElementSibling();
+//        找到智联证书模块全部元素
+        Elements zhiLianCertificatCeEles = root.getElementsMatchingOwnText("^证书$");
+//        找到智联语言能力模块全部元素
+        Elements zhiLianLanguageEles = root.getElementsMatchingOwnText("^语言能力$");
+//        找到智联专业技能模块全部元素
+        Elements zhiLianSkillsEles = root.getElementsMatchingOwnText("^专业技能$");
+        //            获取智联证书说明的元素
+        if (zhiLianCertificatCeEles.size() > 0) {
+            zhiLianCertificatCeEles = zhiLianCertificatCeEles.get(0).parent().parent().nextElementSibling().getElementsByAttributeValue("style", "font-size:9.0pt;font-family:\n  宋体;mso-ascii-font-family:Calibri;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\n  宋体;mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:Calibri;\n  mso-hansi-theme-font:minor-latin");
+        }
+        //            获取智联语言说明的元素
+        if (zhiLianLanguageEles.size() > 0) {
+            zhiLianLanguageEles = zhiLianLanguageEles.get(0).parent().parent().nextElementSibling().getElementsByAttributeValue("style", "font-size:9.0pt;\n  font-family:宋体;mso-ascii-font-family:Calibri;mso-ascii-theme-font:minor-latin;\n  mso-fareast-font-family:宋体;mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:\n  Calibri;mso-hansi-theme-font:minor-latin");
+        }
+        //            获取智联技能说明的元素
+        if (zhiLianSkillsEles.size() > 0) {
+            zhiLianSkillsEles = zhiLianSkillsEles.get(0).parent().parent().nextElementSibling().getElementsByTag("span");
+        }
+//        获取51Job的技能列表
+        if (skills51Eles.size() > 0) {
+            Element educationEleParent = skills51Eles.first().parent().nextElementSibling();
             Elements skillOrCertificateNameEle = educationEleParent.getElementsByAttributeValue("style", "text-align:right;width:120px;font-size:13px;font-weight:bold;color:#666;word-break:break-all;");
             Elements skillProficiencyEle = educationEleParent.getElementsByAttributeValue("class", "skco");
             skillOrCertificateNameEle.stream().forEach(element -> {
@@ -45,12 +63,41 @@ public class SkillsParser extends BaseParser {
                         skills.setCertificateDate(Jsoup.parse(element.toString()).text());
                         skills.setSkillOrCertificateName(element.nextElementSibling().text());
                         list.add(skills);
-
                     });
-                    return list;
-
                 }
             }
+        }
+//        获取智联的证书列表
+        if (zhiLianCertificatCeEles.size() > 0) {
+            for (int i = 0; i < zhiLianCertificatCeEles.size(); i++) {
+                String[] arr = zhiLianCertificatCeEles.get(i).text().split(" ");
+                Skills skills = new Skills();
+                skills.setCertificateDate(arr[0]);
+                skills.setSkillOrCertificateName(arr[1]);
+                list.add(skills);
+            }
+        }
+//        获取智联的语言能力列表
+        if (zhiLianLanguageEles.size() > 0) {
+            for (int i = 0; i < zhiLianLanguageEles.size(); i++) {
+                String[] arr = zhiLianLanguageEles.get(i).text().split("： ");
+                Skills skills = new Skills();
+                skills.setSkillOrCertificateName(arr[0]);
+                skills.setSkillProficiency(arr[1]);
+                list.add(skills);
+            }
+        }
+
+//            获取智联的技能特长列表
+        if (zhiLianSkillsEles.size() > 0) {
+            for (int i = 0; i < zhiLianSkillsEles.size(); i += 2) {
+                String[] arr = zhiLianSkillsEles.get(i).text().split("：");
+                Skills skills = new Skills();
+                skills.setSkillOrCertificateName(arr[0]);
+                skills.setSkillProficiency(arr[1]);
+                list.add(skills);
+            }
+
         }
         return list;
     }
--- src/test/java/com/lds/common/resume/parser/SkillsParserTest.java
+++ src/test/java/com/lds/common/resume/parser/SkillsParserTest.java
@@ -15,15 +15,25 @@ import java.io.File;
 public class SkillsParserTest extends BaseTest {
 
     @Test
-    public void oneEducationParse() throws Exception {
-        File f = new File(basePath + "智联招聘_蒋程_网页设计师_中文_20191031_1572509772676.doc");
+    public void oneEducationParseZhiLian() throws Exception {
+        File f = new File(basePath + "智联招聘_温超平_软件测试_中文_20191105_1572935670709.doc");
         String html = converHtml(f);
 
         SkillsParser parser = new SkillsParser(html);
         parser.parse().forEach(skills -> {
-                    System.out.println(skills.toString()    );
+                    System.out.println(skills.toString());
                 }
+        );
+    }
+    @Test
+    public void oneEducationParse51Job() throws Exception {
+        File f = new File(basePath + "51job_凌梦之(23234904).doc");
+        String html = converHtml(f);
 
-                );
+        SkillsParser parser = new SkillsParser(html);
+        parser.parse().forEach(skills -> {
+                    System.out.println(skills.toString());
+                }
+        );
     }
 }
